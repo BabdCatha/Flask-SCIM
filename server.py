ef403231b7a3a6eace31d5d7079004a19258d74d
BabdCatha <thomascyril0308@gmail.com>
2024-07-13 02:09:25 +0200

    Add /ServiceProviderConfig endpoint

---
+++ server.py
@@ -0,0 +1,54 @@
+from flask import Flask
+from flask import json
+
+app = Flask(__name__)
+
+#Root of the server
+@app.route("/")
+def root():
+	return "/"
+
+##Service Provider Configuration Endpoints (https://datatracker.ietf.org/doc/html/rfc7644#section-4)
+#ServiceProviderConfig
+@app.route("/ServiceProviderConfig", methods=["GET"])
+def ServiceProviderConfig():
+
+	responseDict = {"schemas":["urn:ietf:params:scim:schemas:core:2.0:ServiceProviderConfig"]}
+
+	#Adding the different capabilities of the server
+	#https://datatracker.ietf.org/doc/html/rfc7643#section-5
+	responseDict["patch"] = {"supported":False}
+
+	responseDict["bulk"] = {
+		"supported":False,
+		"maxOperations":0,
+		"maxPayloadSize":0
+	}
+
+	responseDict["filter"] = {
+		"supported":False,
+		"maxResults":0
+	}
+
+	responseDict["changePassword"] = {"supported":False}
+
+	responseDict["etag"] = {"supported":False}
+
+	responseDict["authenticationSchemes"] = []
+	responseDict["authenticationSchemes"].append({
+			"type":"httpbasic",
+			"name":"HTTP Basic Auth",
+			"description":"HTTP Basic Auth Scheme. Base64($Username:$Password)"
+		})
+
+	response = app.response_class(
+		response=json.dumps(responseDict),
+		status=200,
+		mimetype="application/scim+json"
+	)
+
+	return response
+
+#main driver function
+if __name__ == '__main__':
+	app.run()
